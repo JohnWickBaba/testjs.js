77347f458d2d5deb7cf99f9a9a58f1fd7028e2ec
JohnWickBaba <156380128+JohnWickBaba@users.noreply.github.com>
2024-01-24 12:52:41 +0000

    container config

--- .devcontainer/01_basics/variables.js
+++ .devcontainer/01_basics/variables.js
@@ -2,6 +2,10 @@ const accountId = 8933800;
 var accountEmail = "deepak@google.com";
 let accountPassword = "deepak@1";
 accountCity = "Banaras"
+/*
+Prefer not use var, 
+beacuse of issue in block scope and functional scope
+*/
 
 // accountId = 2;
 
