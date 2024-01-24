a0bf2acded3ecfbff0362d8916637c7f93963e5a
JohnWickBaba <156380128+JohnWickBaba@users.noreply.github.com>
2024-01-24 08:46:11 +0000

    container config

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 8933800;
+var accountEmail = "deepak@google.com";
+let accountPassword = "deepak@1";
+accountCity = "Banaras"
+
+// accountId = 2;
+
+accountEmail = dk@gmail.com;
+accountPassword = "7784943";
+accountCity = "delhi";
+console.log(accountId);
+
+/*
+
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity]);
