8ca753d88dc2bad15337c25573c424f3cdb5d31c
JohnWickBaba <156380128+JohnWickBaba@users.noreply.github.com>
2024-01-25 20:48:43 +0000

    container config

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 8933800;
+var accountEmail = "deepak@google.com";
+let accountPassword = "deepak@1";
+accountCity = "Banaras"
+/*
+Prefer not use var, 
+beacuse of issue in block scope and functional scope
+*/
+
+// accountId = 2;
+
+// let accountPassword = "7784943";
+/* accountCity = "delhi";
+console.log(accountId);*/
+console.table([accountId,accountEmail,accountPassword,accountCity]);
