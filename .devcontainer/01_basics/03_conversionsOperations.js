a83946eb83ab718bb172dc319fb471babd5be8a9
JohnWickBaba <156380128+JohnWickBaba@users.noreply.github.com>
2024-01-26 10:14:11 +0000

    container config

--- .devcontainer/01_basics/03_conversionsOperations.js
+++ .devcontainer/01_basics/03_conversionsOperations.js
@@ -1,3 +1,35 @@
-let age = 33
+//___________Coversions________//
+// let age = undefined
 
-console.log(typeof score);
+// console.log(typeof age);
+// console.log(typeof(age));
+// let valueInNumber = Number(age)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+//_________Operations_________//
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**5);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello";
+// let str2 = "deepak";
+// let str3 = str1 + str2;
+// console.log(str3);
+// console.log((3 + 4)* 5 % 3);
+// console.log(+true);
+// console.log(+"");
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
