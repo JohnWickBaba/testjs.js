492eb035343de407cdadb851e81a30ec9a29bbd3
JohnWickBaba <156380128+JohnWickBaba@users.noreply.github.com>
2024-02-02 14:48:02 +0000

    conatiner config

---
+++ 01_basics/05_numsandmaths.js
@@ -0,0 +1,33 @@
+// const score = 40;
+// console.log(score);
+// const balance = Number(20);
+// console.log(balance);
+
+// console.log(balance.toFixed(5));
+// console.log(balance.toString().length);
+
+// const otherNumber = 123.8769;
+// console.log(otherNumber.toPrecision(3));
+
+// const bigNumber = 10000000000;
+// console.log(bigNumber.toLocaleString());
+// console.log(bigNumber.toLocaleString("en-IN"));
+
+
+// ======================MATHS=====================
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 5, 3, 7, 8));
+// console.log(Math.max(4, 5, 3, 7, 8));
+console.log(Math.random());
+console.log(Math.floor(Math.random()*4.3) +1);
+
+const min = 10;
+const max = 50;
+console.log(Math.floor(Math.random() * (max - min +1))  + min); 
+
+
