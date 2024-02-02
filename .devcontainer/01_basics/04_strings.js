4386956bc93a3427d10f43b5d801e83dd9fa312c
JohnWickBaba <156380128+JohnWickBaba@users.noreply.github.com>
2024-02-02 13:38:33 +0000

    container config

---
+++ .devcontainer/01_basics/04_strings.js
@@ -0,0 +1,9 @@
+const name = "deepak";
+const batted = 20;
+console.log(`hello my name is ${name} & i batted ${batted} times`);
+const fakeName = "      dkbaba              ";
+console.log(fakeName.trim());
+console.log(fakeName);
+const newStr = "kuttekpille";
+console.log(newStr.replace("pille","jaan"));
+console.log(newStr.split("k"));
