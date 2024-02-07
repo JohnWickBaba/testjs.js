82844d9619bd1a4cd2e194520d7bb43524489c98
JohnWickBaba <156380128+JohnWickBaba@users.noreply.github.com>
2024-02-07 17:29:06 +0000

    config container

---
+++ 01_basics/06_dateandtime.js
@@ -0,0 +1,19 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString);
+// console.log(myDate.toISOString);
+// console.log(myDate.toJSON);
+// console.log(myDate.toLocaleDateString);
+// console.log(myDate.toLocaleTimeString);
+
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3);
+// console.log(myCreatedDate.toLocaleString());
+// let iCreatedDate = new Date("2023-01-14");
+// let iCreatedDate = new Date("01-14-2023");
+// console.log(iCreatedDate.toLocaleString());
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(Math.floor(Date.now()/1000));
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth());
